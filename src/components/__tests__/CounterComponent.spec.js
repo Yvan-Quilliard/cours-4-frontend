5ef5ec2dbda1877ea2cbaf2e1a4c5aa421cdb255
Yvan <yvanquilliard@hotmail.fr>
2024-01-29 17:32:18 +0100

    fix git hook

--- src/components/__tests__/CounterComponent.spec.js
+++ src/components/__tests__/CounterComponent.spec.js
@@ -34,8 +34,10 @@ describe('Counter', () => {
       expect(counterStore.increment).toHaveBeenCalledTimes(1)
     })
     it('decrements counter', async () => {
-      // const wrapper = mountCounter(50)
-      // const counterStore = useCounterStore()
+      const wrapper = mountCounter(50)
+      const counterStore = useCounterStore()
+      await wrapper.find('button[id=decrement]').trigger('click')
+      expect(counterStore.decrement).toHaveBeenCalledTimes(1)
     })
   })
 })
